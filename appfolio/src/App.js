214e80705998686e2f84fe0a173f093837560d02
Anas Hussain <anashussain.prystech@gmail.com>
2024-02-21 04:57:02 +0500

    initial commit cra

---
+++ appfolio/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+
+  const URLData = "eyJhc3NldCI6eyJjaXR5IjoiTWFuaGF0dGFuIiwic3RhdGUiOiJOZXcgWW9yayIsInppcCI6IjEwMDEzIiwic3RyZWV0QWRkcmVzcyI6IjU4OCBCcm9hZHdheSAiLCJ0eXBlIjoiQ29sZCBTdG9yYWdlIn0sInVzZXIiOnsiZW1haWwiOiJwYXJ0bmVyYUBjb21wYW55LmNvbSIsImZpcnN0TmFtZSI6IkplZmYiLCJsYXN0TmFtZSI6Ik1vcnJpcyJ9LCJzb3VyY2UiOiJBcHBmb2xpbyIsImNvbXBhbnkiOiJOZXcgR3JlYXQgQ29tcGFueSIsImZvcm1EYXRhUHJlZmlsbCI6eyJsb2FuVHlwZSI6IkNvbnN0cnVjdGlvbiIsInZhbHVhdGlvbiI6MTIzMywibmV0T3BlcmF0aW5nSW5jb21lIjoxMjMzMDAsInJlbm92YXRpb25Db3N0cyI6MjIwMDAsInByb2plY3RlZE5ldE9wZXJhdGluZ0luY29tZSI6MTc1MDAwLCJsYW5kUHJpY2UiOjE1NDAwMDAsImNvbnN0cnVjdGlvbkJ1ZGdldCI6NDUwMDAwLCJleHBlbnNlc1NwZW50VG9EYXRlIjo0NTAwMCwiZGVzaXJlZExldmVyYWdlIjo3MH19"; 
+  const JSONData = JSON.parse(atob(URLData));
+
+  return (
+    <div className="app">
+      <Header data={JSONData}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ appfolio/src/Header.js
@@ -0,0 +1,32 @@
+import levLogo from './assets/Logo.svg';
+import appfolioLogo from './assets/appfolio-logo.png.svg';
+
+function Header ({data}) {
+    const { asset , user } = data;
+    console.log(asset);
+    return(
+        
+        <header className="header">
+            <div className="inner">
+                <div className="logos">
+                    <img src={levLogo} alt="" className="lev-logo" />
+                    <span>x</span>
+                    <img src={appfolioLogo} alt="" className="appfolio-logo" />
+                </div>
+                <div className="user-data">
+                    <div className="name-step2">{`${user.firstName} ${user.lastName}`}</div>
+                    <div className="address">
+                        <div className="line1">{asset.streetAddress}</div>
+                        <div className="line2">{`${asset.city}, ${asset.state}, ${asset.zip}`}</div>
+                    </div>
+                    <div className="creds">
+                        <div className="name">{`${user.firstName} ${user.lastName}`}</div>
+                        <div className="email">{user.email}</div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
